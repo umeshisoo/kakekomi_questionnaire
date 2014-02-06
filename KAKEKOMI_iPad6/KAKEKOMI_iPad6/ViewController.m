f72ce6a4c1415f37f6a68b87a535fc9246c58f3e
ryosuke <tanukiok@i.softbank.jp>
2014-02-07 05:26:16 +0900

    sample

---
+++ KAKEKOMI_iPad6/KAKEKOMI_iPad6/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  KAKEKOMI_iPad6
+//
+//  Created by Ryosuke OKUHTA on 2014/01/27.
+//  Copyright (c) 2014年 Ryosuke OKUHTA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *calender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    ///
+
+}
+
+@end
